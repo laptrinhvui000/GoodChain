465a48f128b06ac7d35f00e2c2d263b1ed9e9862
mlibre <m.gh@linuxmail.org>
2022-03-27 14:34:24 +0430

    eslint

--- example/index.js
+++ example/index.js
@@ -1,25 +1,25 @@
-const GoodChain = require( "../main.js" );
-const path = require( "path" );
-const fs = require( "fs" );
+const GoodChain = require( "../main.js" )
+const path = require( "path" )
+const fs = require( "fs" )
 const { inspect } = require( "util" );
 
 ( async () =>
 {
-	const scriptPath = path.dirname( process.argv[1] );
+	const scriptPath = path.dirname( process.argv[1] )
 
-	const userKeysPath = path.join( scriptPath, "./my_keys/" );
-	const userPublicKey = fs.readFileSync( path.join( userKeysPath, "public_key.pem" ), "utf8" );
-	const userPrivateKey = fs.readFileSync( path.join( userKeysPath, "private_key.pem" ), "utf8" );
+	const userKeysPath = path.join( scriptPath, "./my_keys/" )
+	const userPublicKey = fs.readFileSync( path.join( userKeysPath, "public_key.pem" ), "utf8" )
+	const userPrivateKey = fs.readFileSync( path.join( userKeysPath, "private_key.pem" ), "utf8" )
 
-	const validatorKeyPath = path.join( scriptPath, "./validator_keys/" );
-	const validatorPublicKey = fs.readFileSync( path.join( validatorKeyPath, "public_key.pem" ), "utf8" );
-	const validatorPrivateKey = fs.readFileSync( path.join( validatorKeyPath, "private_key.pem" ), "utf8" );
+	const validatorKeyPath = path.join( scriptPath, "./validator_keys/" )
+	const validatorPublicKey = fs.readFileSync( path.join( validatorKeyPath, "public_key.pem" ), "utf8" )
+	const validatorPrivateKey = fs.readFileSync( path.join( validatorKeyPath, "private_key.pem" ), "utf8" )
 	const validator = {
 		publicKey: validatorPublicKey,
 		privateKey: validatorPrivateKey
-	};
+	}
 
-	const chain = await new GoodChain( { validator } );
+	const chain = await new GoodChain( { validator } )
 
 	const new_transaction = {
 		index: 1, // userPublicKey N transaction
@@ -28,41 +28,41 @@ const { inspect } = require( "util" );
 		amount: 1,
 		fee: 1,
 		tickPrice: 0,
-	};
-	new_transaction.sign = chain.signTransaction( new_transaction, userPrivateKey ); // sign the transaction by the private key of the sender
-	new_transaction.hash = chain.hash( new_transaction );
-	await chain.new_transaction( new_transaction );
-	await chain.new_block();
-	// console.log(chain.chain);
-	console.log( inspect( chain.chain, { colors: true, depth: null } ) );
+	}
+	new_transaction.sign = chain.signTransaction( new_transaction, userPrivateKey ) // sign the transaction by the private key of the sender
+	new_transaction.hash = chain.hash( new_transaction )
+	await chain.new_transaction( new_transaction )
+	await chain.new_block()
+	// console.log(chain.chain)
+	console.log( inspect( chain.chain, { colors: true, depth: null } ) )
 } )();
 
 
 async function full ()
 {
-	const myState = require( "./my_chain/mystate.json" );
-	const myChain = require( "./my_chain/mychain.json" );
-	const myNodes = require( "./my_chain/mynodes.json" );
+	const myState = require( "./my_chain/mystate.json" )
+	const myChain = require( "./my_chain/mychain.json" )
+	const myNodes = require( "./my_chain/mynodes.json" )
 
-	const scriptPath = path.dirname( process.argv[1] );
-	const userKeysPath = path.join( scriptPath, "./my_keys/" );
-	GoodChain.generateKeyPairs( userKeysPath );
+	const scriptPath = path.dirname( process.argv[1] )
+	const userKeysPath = path.join( scriptPath, "./my_keys/" )
+	GoodChain.generateKeyPairs( userKeysPath )
 
-	const userPublicKey = fs.readFileSync( path.join( userKeysPath, "public_key.pem" ), "utf8" );
-	const userPrivateKey = fs.readFileSync( path.join( userKeysPath, "private_key.pem" ), "utf8" );
+	const userPublicKey = fs.readFileSync( path.join( userKeysPath, "public_key.pem" ), "utf8" )
+	const userPrivateKey = fs.readFileSync( path.join( userKeysPath, "private_key.pem" ), "utf8" )
 
-	const validatorKeyPath = path.join( scriptPath, "./validator_keys/" );
-	GoodChain.generateKeyPairs( validatorKeyPath );
+	const validatorKeyPath = path.join( scriptPath, "./validator_keys/" )
+	GoodChain.generateKeyPairs( validatorKeyPath )
 
-	const validatorPublicKey = fs.readFileSync( path.join( validatorKeyPath, "public_key.pem" ), "utf8" );
-	const validatorPrivateKey = fs.readFileSync( path.join( validatorKeyPath, "private_key.pem" ), "utf8" );
+	const validatorPublicKey = fs.readFileSync( path.join( validatorKeyPath, "public_key.pem" ), "utf8" )
+	const validatorPrivateKey = fs.readFileSync( path.join( validatorKeyPath, "private_key.pem" ), "utf8" )
 	const validator = {
 		publicKey: validatorPublicKey,
 		privateKey: validatorPrivateKey
-	};
+	}
 
-	const chain = await new GoodChain( { validator, dbPath:"./", state: myState, chain: myChain, nodes: myNodes } );
-	// console.log(chain.chain);
+	const chain = await new GoodChain( { validator, dbPath: "./", state: myState, chain: myChain, nodes: myNodes } )
+	// console.log(chain.chain)
 
 	const new_transaction = {
 		index: 1, // userPublicKey N transaction
@@ -71,11 +71,11 @@ async function full ()
 		amount: 1,
 		fee: 1,
 		tickPrice: 0,
-	};
-	new_transaction.sign = chain.signTransaction( new_transaction, userPrivateKey ); // sign the transaction by the private key of the sender
-	new_transaction.hash = chain.hash( new_transaction );
-	await chain.new_transaction( new_transaction );
-	await chain.new_block();
-	// console.log(chain.chain);
-	console.log( inspect( chain.chain, { colors: true, depth: null } ) );
+	}
+	new_transaction.sign = chain.signTransaction( new_transaction, userPrivateKey ) // sign the transaction by the private key of the sender
+	new_transaction.hash = chain.hash( new_transaction )
+	await chain.new_transaction( new_transaction )
+	await chain.new_block()
+	// console.log(chain.chain)
+	console.log( inspect( chain.chain, { colors: true, depth: null } ) )
 }
--- main.js
+++ main.js
@@ -12,41 +12,41 @@ class GoodChain
 {
 	constructor ( { chain, state, nodes, dbPath, validator } = {} )
 	{
-		const self = this;
-		self.chain = chain || []; // block chain
-		self.state = state || {}; // state of the blockchain
-		self.nodes = nodes || []; // list of nodes, validators or miners
-		self.transactions_pool = []; // list of current transactions for the next candidate block
-		self.blockReward = 30; // Block reward for successfully mining a block in GCT
-		self.validationFee = 0.25; // The percentage of MCT that a validator will pay to validate a block
-		self.feesToCharity = 0.25; // The percentage of the block's fee that will be sent to the charity
-		self.rewardToCharity = 0.2; // The percentage of the block's reward that will be sent to the charity
-		self.charityAddress = "2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d4947664d413047435371475349623344514542415155414134474e4144434269514b426751445a694c7247666d464850737a3066436f703867373675334a320a62587768576a313267474a2f6756346a7954375468692b3872386143712b495448494c695156614b546238436f4c693632305a4e502f7573754a7a4f5949384c0a5334696955366e334355387164434d5a6448496d5461754a6f444767715932527564436c522f3431576b676545752f6d34327635346c315a3967595136666a470a626d642f474f52446d394f754573787138514944415141420a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a"; // Charity address
+		const self = this
+		self.chain = chain || [] // block chain
+		self.state = state || {} // state of the blockchain
+		self.nodes = nodes || [] // list of nodes, validators or miners
+		self.transactions_pool = [] // list of current transactions for the next candidate block
+		self.blockReward = 30 // Block reward for successfully mining a block in GCT
+		self.validationFee = 0.25 // The percentage of MCT that a validator will pay to validate a block
+		self.feesToCharity = 0.25 // The percentage of the block's fee that will be sent to the charity
+		self.rewardToCharity = 0.2 // The percentage of the block's reward that will be sent to the charity
+		self.charityAddress = "2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d4947664d413047435371475349623344514542415155414134474e4144434269514b426751445a694c7247666d464850737a3066436f703867373675334a320a62587768576a313267474a2f6756346a7954375468692b3872386143712b495448494c695156614b546238436f4c693632305a4e502f7573754a7a4f5949384c0a5334696955366e334355387164434d5a6448496d5461754a6f444767715932527564436c522f3431576b676545752f6d34327635346c315a3967595136666a470a626d642f474f52446d394f754573787138514944415141420a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a" // Charity address
 
-		self.executeScriptPath = dirname( process.argv[1] );
-		self.dbPath = dbPath || self.executeScriptPath; // path of the database files. state.json and chain.json
+		self.executeScriptPath = dirname( process.argv[1] )
+		self.dbPath = dbPath || self.executeScriptPath // path of the database files. state.json and chain.json
 		return ( async () =>
 		{
 			// initialize the validator object
-			self.validator = await self.init_validator( validator );
+			self.validator = await self.init_validator( validator )
 			// Initialize the chain and state databases
-			await self.init_database();
+			await self.init_database()
 			// Creating the genesis block
 			if ( self.chain.length == 0 )
 			{
-				await self.genesis_block();
+				await self.genesis_block()
 			}
-			return self;
+			return self
 		} )();
 	}
 
 	// Mine a new block and add it to the chain
 	async new_block ( { index, previous_hash } = {} )
 	{
-		const self = this;
+		const self = this
 		if ( typeof index == "undefined" )
 		{
-			index = self.last_block().index + 1;
+			index = self.last_block().index + 1
 		}
 		const block = {
 			index, // block number
@@ -58,8 +58,8 @@ class GoodChain
 			validator_address: self.validator.address // validator address in hex
 		};
 
-		await self.add_block( block );
-		return block;
+		await self.add_block( block )
+		return block
 	}
 
 	// Add an existing block to the chain
@@ -67,48 +67,48 @@ class GoodChain
 	// and the state_hash of the block, and the hash of the block if it is not calculated yet
 	async add_block ( block )
 	{
-		const self = this;
+		const self = this
 		try
 		{
-			await self.pre_validate_block( block );
-			self.state_update( block );
-			block.state_hash ||= self.hash( self.state ); // for when THIS validator mines a block
-			block.validator_sign ||= self.signBlock( block ); // for when THIS validator mines a block
-			block.hash ||= self.hash( block ); // for when THIS validator mines a block
-			self.validate_block( block );
-			self.chain.push( block );
-			self.transactions_pool = [];
-			await self.save(); // save the state and the chain
+			await self.pre_validate_block( block )
+			self.state_update( block )
+			block.state_hash ||= self.hash( self.state ) // for when THIS validator mines a block
+			block.validator_sign ||= self.signBlock( block ) // for when THIS validator mines a block
+			block.hash ||= self.hash( block ) // for when THIS validator mines a block
+			self.validate_block( block )
+			self.chain.push( block )
+			self.transactions_pool = []
+			await self.save() // save the state and the chain
 			return block;
 		}
 		catch ( error )
 		{
-			await self.load();
-			console.error( error );
-			throw error;
+			await self.load()
+			console.error( error )
+			throw error
 		}
 	}
 
 	// update the state of the node
 	state_update ( block )
 	{
-		const self = this;
-		const vAddress = block.validator_address;
-		self.init_addresses( vAddress, self.charityAddress );
-		// Update state of the validator_address who mined the block.
-		self.state[vAddress].GCT += self.blockReward - self.blockReward * self.rewardToCharity;
-		self.state[vAddress].MCT -= self.state[vAddress].MCT * self.validationFee;
-		self.state[self.charityAddress].GCT += self.blockReward * self.rewardToCharity;
+		const self = this
+		const vAddress = block.validator_address
+		self.init_addresses( vAddress, self.charityAddress )
+		// Update state of the validator_address who mined the block
+		self.state[vAddress].GCT += self.blockReward - self.blockReward * self.rewardToCharity
+		self.state[vAddress].MCT -= self.state[vAddress].MCT * self.validationFee
+		self.state[self.charityAddress].GCT += self.blockReward * self.rewardToCharity
 
 		// update the balances of the accounts indicated in the transactions
 		for ( let index = 0; index < block.transactions.length; index++ )
 		{
-			const element = block.transactions[index];
-			self.state[vAddress].GCT += element.fee - element.fee * self.feesToCharity;
-			self.state[self.charityAddress].GCT += element.fee * self.feesToCharity;
-			self.state[element.from].GCT -= element.fee;
-			self.state[element.from].GCT -= element.amount;
-			self.state[element.to].GCT += element.amount;
+			const element = block.transactions[index]
+			self.state[vAddress].GCT += element.fee - element.fee * self.feesToCharity
+			self.state[self.charityAddress].GCT += element.fee * self.feesToCharity
+			self.state[element.from].GCT -= element.fee
+			self.state[element.from].GCT -= element.amount
+			self.state[element.to].GCT += element.amount
 		}
 		// validators update their state by exceuting the transactions
 		// how ever they can download it as well
@@ -121,21 +121,21 @@ class GoodChain
 
 		for ( let index = 0; index < block.transactions.length; index++ )
 		{
-			const element = block.transactions[index];
-			await self.validate_transaction( element );
+			const element = block.transactions[index]
+			await self.validate_transaction( element )
 		}
 	}
 
 	validate_block ( block )
 	{
 		const self = this;
-		const block_without_hash = _.cloneDeep( block );
+		const block_without_hash = _.cloneDeep( block )
 		delete block_without_hash.hash;
 
 		// Check if the hash of the block is correct
 		if ( block.hash != self.hash( block_without_hash ) )
 		{
-			throw new Error( "Block hash is not valid" );
+			throw new Error( "Block hash is not valid" )
 		}
 		const last_block = self.last_block();
 
@@ -146,11 +146,11 @@ class GoodChain
 		}
 		if ( last_block.hash != block.previous_hash )
 		{
-			throw new Error( "Previous hash is not valid" );
+			throw new Error( "Previous hash is not valid" )
 		}
 		if ( block.timestamp < last_block.timestamp )
 		{
-			throw new Error( "Block timestamp is not valid" );
+			throw new Error( "Block timestamp is not valid" )
 		}
 		if ( block.index != last_block.index + 1 )
 		{
@@ -158,28 +158,28 @@ class GoodChain
 		}
 		if ( block.block_reward != self.blockReward )
 		{
-			throw new Error( "Block reward is not valid" );
+			throw new Error( "Block reward is not valid" )
 		}
 		if ( block.state_hash != self.hash( self.state ) )
 		{
-			throw new Error( "Block state hash is not valid" );
+			throw new Error( "Block state hash is not valid" )
 		}
 
-		const block_without_hash_sign = _.cloneDeep( block_without_hash );
+		const block_without_hash_sign = _.cloneDeep( block_without_hash )
 		delete block_without_hash_sign.validator_sign;
 		if ( !self.checkBlockSign( block_without_hash_sign, block.validator_sign, self.validator.publicKey ) )
 		{
-			throw new Error( "Block sign is not valid" );
+			throw new Error( "Block sign is not valid" )
 		}
 	}
 
 	// Adds a new transaction to the list of transactions
 	new_transaction ( { index, from, to, amount, fee, tickPrice, hash, sign } )
 	{
-		const self = this;
-		from = GoodChain.hex( from );
-		to = GoodChain.hex( to );
-		self.validate_transaction( { index, from, to, amount, fee, tickPrice, hash, sign } );
+		const self = this
+		from = GoodChain.hex( from )
+		to = GoodChain.hex( to )
+		self.validate_transaction( { index, from, to, amount, fee, tickPrice, hash, sign } )
 		self.transactions_pool.push( {
 			index, // only way to stop a signed transaction from being broadcasted again in future blocks
 			// this is the user transaction number. for example 10 means this transaction is the 10th transaction
@@ -196,15 +196,15 @@ class GoodChain
 			hash // hash of the transaction
 		} );
 		// returns block number where this transaction will be included
-		return self.last_block().index + 1;
+		return self.last_block().index + 1
 	}
 
 
 	validate_transaction ( { index, from, to, amount, fee, tickPrice, hash, sign } )
 	{
-		const self = this;
-		from = GoodChain.hex( from );
-		to = GoodChain.hex( to );
+		const self = this
+		from = GoodChain.hex( from )
+		to = GoodChain.hex( to )
 		const trxWithoutHashAndSign = {
 			index,
 			from,
@@ -212,15 +212,15 @@ class GoodChain
 			amount,
 			fee,
 			tickPrice,
-		};
-		self.init_addresses( from, to );
+		}
+		self.init_addresses( from, to )
 		if ( index != self.account_last_index( from ) + 1 )
 		{
-			throw new Error( "Transaction index is not valid" );
+			throw new Error( "Transaction index is not valid" )
 		}
 		if ( !self.checkTransactionSign( trxWithoutHashAndSign, sign, GoodChain.hex( from, "hex", "utf8" ) ) )
 		{
-			throw new Error( "Transaction sign is not valid" );
+			throw new Error( "Transaction sign is not valid" )
 		}
 		// Check if the transaction is valid
 		// check if the index value is valid, mean index value should be the same or lower than the last one
@@ -246,20 +246,20 @@ class GoodChain
 	// Hashes a Block
 	hash ( obj )
 	{
-		return hash( obj, { algorithm: "sha3-512" } );
+		return hash( obj, { algorithm: "sha3-512" } )
 	}
 
 	static hex ( string, from = "utf8", to = "hex" )
 	{
 		if ( string.startsWith( "-----BEGIN" ) && from == "utf8" && to == "hex" )
 		{
-			return Buffer.from( string, from ).toString( to );
+			return Buffer.from( string, from ).toString( to )
 		}
 		else if ( from == "hex" && to == "utf8" )
 		{
-			return Buffer.from( string, from ).toString( to );
+			return Buffer.from( string, from ).toString( to )
 		}
-		return string;
+		return string
 	}
 
 	// Signs a Block by the validator privateKey
@@ -267,26 +267,26 @@ class GoodChain
 	// other's addresses, because that will minus some MCT from them
 	signBlock ( block )
 	{
-		const self = this;
-		const shaHash = self.hash( block );
+		const self = this
+		const shaHash = self.hash( block )
 
 		const encrypted = crypto.privateEncrypt( {
 			key: self.validator.privateKey,
 			padding: crypto.constants.RSA_NO_PADDING
-		}, Buffer.from( shaHash, "utf8" ) );
-		return encrypted.toString( "base64" );
+		}, Buffer.from( shaHash, "utf8" ) )
+		return encrypted.toString( "base64" )
 	}
 
 	// Checks if the block is signed by the validator
 	checkBlockSign ( block, sign, publicKey )
 	{
-		const self = this;
-		const shaHash = self.hash( block );
+		const self = this
+		const shaHash = self.hash( block )
 		const decrypted = crypto.publicDecrypt( {
 			key: publicKey,
 			padding: crypto.constants.RSA_NO_PADDING
-		}, Buffer.from( sign, "base64" ) );
-		return decrypted.toString() == shaHash;
+		}, Buffer.from( sign, "base64" ) )
+		return decrypted.toString() == shaHash
 	}
 
 	// Sign a transaction using privateKey
